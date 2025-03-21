62b69cace68ffef65287d4417bbb09c991b0e140
marcela gottlieb <marcela.gottlieb@totalexpress.com.br>
2025-03-21 15:37:29 -0300

    Base do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, FormsModule], // Import necessário para *ngIf e ngModel
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gerador-senhas';
+  tamanhoSenha: number = 8;
+  usarMaiusculas: boolean = false;
+  usarNumeros: boolean = false;
+  usarSimbolos: boolean = false;
+  senhaGerada: string = '';
+
+  gerarSenha() {
+    const minusculas = 'abcdefghijklmnopqrstuvwxyz';
+    const maiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numeros = '0123456789';
+    const simbolos = '!@#$%^&*()_+[]{}|;:,.<>?';
+
+    let caracteres = minusculas;
+    if (this.usarMaiusculas) caracteres += maiusculas;
+    if (this.usarNumeros) caracteres += numeros;
+    if (this.usarSimbolos) caracteres += simbolos;
+
+    this.senhaGerada = this.gerarStringAleatoria(caracteres, this.tamanhoSenha);
+  }
+
+  gerarStringAleatoria(base: string, comprimento: number): string {
+    let resultado = '';
+    for (let i = 0; i < comprimento; i++) {
+      const randomIndex = Math.floor(Math.random() * base.length);
+      resultado += base[randomIndex];
+      
+    }
+   return resultado
+  }
+
+  copiarSenha() {
+    navigator.clipboard.writeText(this.senhaGerada).then(() => {
+      alert('Senha copiada para a área de transferência!');
+    });
+  }
 }
